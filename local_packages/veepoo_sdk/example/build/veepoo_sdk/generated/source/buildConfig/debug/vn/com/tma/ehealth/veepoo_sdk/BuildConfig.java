3a178d9d7134dd2bc2d8e46dcafde950e346226b
Amir Farmanesh <amir.farmanesh@gmail.com>
2024-03-11 11:26:25 +0100

    master updated

---
+++ local_packages/veepoo_sdk/example/build/veepoo_sdk/generated/source/buildConfig/debug/vn/com/tma/ehealth/veepoo_sdk/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vn.com.tma.ehealth.veepoo_sdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "vn.com.tma.ehealth.veepoo_sdk";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ local_packages/veepoo_sdk/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<veepoo_sdk/VeepooSdkPlugin.h>)
+#import <veepoo_sdk/VeepooSdkPlugin.h>
+#else
+@import veepoo_sdk;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [VeepooSdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"VeepooSdkPlugin"]];
+}
+
+@end
