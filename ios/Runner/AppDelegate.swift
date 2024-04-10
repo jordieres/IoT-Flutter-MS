a0237cfec767df15b0b9c8c9981bc54a39a90eca
Amir Farmanesh <amir.farmanesh@gmail.com>
2024-04-10 14:43:05 +0200

    initial work on ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,20 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "com.example.healthywear/metawear",
+                                      binaryMessenger: controller.binaryMessenger)
+    
+    channel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      // Check the method being called
+      if call.method == "connectToDevice" {
+        // Implement your iOS-specific logic here
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
